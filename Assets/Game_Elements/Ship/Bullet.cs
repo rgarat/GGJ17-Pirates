2d4c3d747c9920d2f3e0da6a8b0040157419f840
Ruben Garat <ruben01@gmail.com>
2017-01-22 00:53:40 -0300

    added more particles, added collider to scenery

--- Assets/Game_Elements/Ship/Bullet.cs
+++ Assets/Game_Elements/Ship/Bullet.cs
@@ -6,18 +6,30 @@ public class Bullet : MonoBehaviour {
 	public int damage;
 	public float minY;
 	public GameObject particleHit;
+    public GameObject particleWater;
 
 	void Update(){
-		if (transform.position.y < minY) {
+		if (transform.position.y < minY)
+		{
+		    GameObject.Instantiate(particleWater, transform.position, Quaternion.identity);
 			Destroy (gameObject);
 		}
 	}
 
 	void OnTriggerEnter (Collider t){
-		if (t.gameObject.GetComponent<Life>() != null && t.gameObject.tag != gameObject.tag) {
-			Instantiate (particleHit, transform.position, transform.rotation);
-			t.gameObject.GetComponent<Life> ().life_rest(damage);
-			Destroy (gameObject);
-		}
+	    if (t.gameObject.GetComponent<Life>() != null)
+	    {
+	        if (t.gameObject.tag != gameObject.tag)
+	        {
+	            Instantiate(particleHit, transform.position, transform.rotation);
+	            t.gameObject.GetComponent<Life>().life_rest(damage);
+	            Destroy(gameObject);
+	        }
+	    }
+	    else
+	    {
+	        Instantiate(particleHit, transform.position, transform.rotation);
+	        Destroy(gameObject);
+	    }
 	}
 }
